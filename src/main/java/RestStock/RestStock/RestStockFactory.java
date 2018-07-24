66cb7af596cb061bc2883a5c40c0cf11942384d1
Abhishek Priyankar <C50628A@expdagdev.local>
2018-07-24 05:15:33 -0700

    Projest Structure Initial

---
+++ src/main/java/RestStock/RestStock/RestStockFactory.java
@@ -0,0 +1,11 @@
+package RestStock.RestStock;
+
+
+public class RestStockFactory 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hey There is nothing to do!!  HEy HeY");
+    }
+}
