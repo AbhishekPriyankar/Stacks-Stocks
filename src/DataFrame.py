4afeb4c5ff80a98cc7c533e25b0712d68b5c3259
Abhishek Priyankar <abhi_717@hotmail.com>
2019-02-13 23:36:56 +0530

    Pyspark Exemple on FIFA

--- src/DataFrame.py
+++ src/DataFrame.py
@@ -3,7 +3,14 @@ from pyspark.conf import SparkConf
 
 session = SparkSession.builder.master("local").appName("MyPySpark").config(conf=SparkConf()).getOrCreate()
 
-fifaDf = session.read.csv("c:/users/abhishek priyankar/pycharmprojects/stacks-stocks/resources/data.csv")
+fifaDf = session.read.csv("c:/users/abhishek priyankar/pycharmprojects/stacks-stocks/resources/data.csv", header=True)
+
+basicStatDf = fifaDf.select(["ID", "Name", "Age", "Nationality", "Overall", "Club", "Preferred Foot", "Position"])
+fifaDf.show(5)
+basicStatDf.show(5)
+
+jsonDf = basicStatDf.toJSON
+
 
 print(fifaDf.count())
 
