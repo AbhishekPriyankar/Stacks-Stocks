e00883eb9818db8a0b4467ee84f297021cafafda
Abhishek Priyankar <abhi_717@hotmail.com>
2019-02-19 21:43:53 +0530

    Pyspark Exemple on FIFA-Playing Around

--- src/DataFrame.py
+++ src/DataFrame.py
@@ -1,16 +1,23 @@
 from pyspark.sql import *
-from pyspark.conf import SparkConf
+from pyspark import SparkConf, SparkContext
 
 session = SparkSession.builder.master("local").appName("MyPySpark").config(conf=SparkConf()).getOrCreate()
 
-fifaDf = session.read.csv("c:/users/abhishek priyankar/pycharmprojects/stacks-stocks/resources/data.csv", header=True)
+fifaDf = session.read.csv("c:/users/abhishek priyankar/pycharmprojects/stacks-stocks/resources/data.csv",
+                          header=True,
+                          inferSchema=True)
 
 basicStatDf = fifaDf.select(["ID", "Name", "Age", "Nationality", "Overall", "Club", "Preferred Foot", "Position"])
 fifaDf.show(5)
+fifaDf.printSchema()
+
+print("No of Partitions : " + str(fifaDf.rdd.getNumPartitions()))
+fifaDfFivePart = fifaDf.rdd.repartition(5)
+print("Re - Partitions Size : " + str(fifaDfFivePart.getNumPartitions()))
 basicStatDf.show(5)
+print(basicStatDf.dtypes)
 
-jsonDf = basicStatDf.toJSON
-
+jsonDf = basicStatDf.toJSON()
 
 print(fifaDf.count())
 
