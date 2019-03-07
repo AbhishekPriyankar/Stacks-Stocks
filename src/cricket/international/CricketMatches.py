9e4cab592a212219391c91f22a5ad5b67937bb96
Abhishek Priyankar <abhi_717@hotmail.com>
2019-03-07 21:36:35 +0530

    Cricket Data Added and Read CSV as DF

--- src/cricket/international/CricketMatches.py
+++ src/cricket/international/CricketMatches.py
@@ -1,6 +1,11 @@
+from dask.array.chunk import trim
+from pyparsing import col
+
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
 from src.util.Constants import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
 session = SparkSession.builder.master("local").appName("ODI").config(conf=SparkConf()).getOrCreate()
 
@@ -10,3 +15,13 @@ df_lab = session.read.csv(LABELLED_DATASET_PATH, header=True)
 df_orig = session.read.csv(ORIGINAL_DATASET_PATH, header=True)
 
 df_orig.show(5)
+
+# Replace spaces with underscore
+exprs = [col(column).alias(column.replace(' ', '_')) for column in df_orig.columns]
+
+df_org = df_orig.select(*exprs)
+
+df_org.printSchema()
+
+print((df_org.select(df_org["Winner"] == "England").alias("winner")).show(5))
+print(df_org.count())
