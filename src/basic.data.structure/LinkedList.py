187fd5221ce54687d19109fc3193f1637a710c5d
Abhishek Priyankar <abhi_717@hotmail.com>
2019-07-30 11:04:24 +0530

    Linked List

---
+++ src/basic.data.structure/LinkedList.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print (temp.data)
+            temp = temp.next
+
+    def push(self,new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insertAfter(self,key,new_data):
+        new_node = Node(new_data)
+        new_node.next = key.next
+        key.next = new_node
+
+    def append(self, new_data):
+        new_node = Node(new_data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last = self.head
+        while last.next:
+            last = last.next
+
+        last.next = new_node
+
+
+
+
+if __name__ == '__main__':
+    llist = LinkedList()
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    llist.head.next = second
+
+    second.next = third
+    llist.append(6)
+
+    llist.printList()
+
